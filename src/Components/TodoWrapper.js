9c8855ece9049477d585c6923d423629d2c1f6f2
Vlad <vlad.kylakov81@gmail.com>
2024-06-04 17:30:46 +0300

    Update TodoWrapper.js Added the ability to save data both to a separate file and to local storage and load it when the page is refreshed.

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,9 +1,20 @@
-import React, {useState, useEffect } from 'react'
-import { TodoForm } from './TodoForm'
-import {v4 as uuidv4} from 'uuid';
+import React, { useState, useEffect } from 'react';
+import { TodoForm } from './TodoForm';
+import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
+import { saveAs } from 'file-saver';
 uuidv4();
+
+const exportTodosToFile = (todos) => {
+    if (todos.length === 0) {
+        alert('Make a choice');
+        return;
+    }
+    const json = JSON.stringify(todos, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    saveAs(blob, 'New list.json');
+};
 
 export const TodoWrapper = () => {
     const [todos, setTodos] = useState(() => {
@@ -15,38 +26,44 @@ export const TodoWrapper = () => {
         localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
 
-    const addTodo = todo =>{
-    setTodos([...todos, {id: uuidv4(), task: todo,
-    complete: false, isEditing: false}])
-    }
+    const addTodo = (todo) => {
+        setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+    };
 
-    const toggleComplete = id => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo ))}
+    const toggleComplete = (id) => {
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+    };
 
-    const deleteTodo = id => {
-    setTodos(todos.filter(todo => todo.id !== id))}
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
+    const editTodo = (id) => {
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo)));
+    };
 
-    const editTodo = id =>
-    setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
-  
     const editTask = (task, id) => {
-    setTodos(todos.map(todo =>  todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
-    }
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo)));
+    };
 
     return (
-    <div className='TodoWrapper'>
-        <h1>Time to shop</h1>
-        <TodoForm addTodo = {addTodo} />
-        {todos.map((todo, index) => (
-        todo.isEditing ?(
-            <EditTodoForm editTodo={editTask} task={todo}/>
-            ) : (
-                <Todo task={todo} key={index}
-            toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
+        <div className='TodoWrapper'>
+            <h1>Time to shop</h1>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo) =>
+                todo.isEditing ? (
+                    <EditTodoForm editTodo={editTask} task={todo} key={todo.id} />
+                ) : (
+                    <Todo
+                        task={todo}
+                        key={todo.id}
+                        toggleComplete={toggleComplete}
+                        deleteTodo={deleteTodo}
+                        editTodo={editTodo}
+                    />
                 )
-            
-            ))}
-    </div>
-  )
-}
+            )}
+            <button className='export-button' onClick={() => exportTodosToFile(todos)}>Download List</button> {/* Передача todos в функцию */}
+        </div>
+    );
+};
